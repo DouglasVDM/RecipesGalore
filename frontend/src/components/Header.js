a74e8a884b54a6cbab0cd5f20ed3798ee15eb05c
tersiakoetzee <tersiakoetzee@gmail.com>
2024-02-07 14:49:17 +0200

    redid home and navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,12 +5,12 @@ import SignIn from "./SignIn";
 
 export const Header = () => {
   return (
-    <Navbar bg="#26A69A" className="h-100 w-100 text-light">
+    <Navbar className="h-100 w-100 text-light">
       <Navbar.Brand href="#home">
         <img src="/Group 7.png" alt="logo" className="logo img-fluid py-2 mx-2 my-2 w-25 h-25" />
       </Navbar.Brand>
       <Nav className="ml-auto">
-      <h6 className="text-center py-4 px-4 text-light lh-lg">
+      <h6 className="text-center py-4 px-4 text-dark lh-lg">
           Search recipes from all over the world. <br></br>Create an account to
           save your favorite recipes. ✨<br></br>
         </h6>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 
 const Home = () => {
@@ -7,120 +7,73 @@ const Home = () => {
   const [diet, setDiet] = useState("none");
   const [exclude, setExclude] = useState("");
   const [response, setResponse] = useState(null);
-  const [recipeInfo, setRecipeInfo] = useState(null);
 
-  const SPOONACULAR_API_KEY = "13a1325476ec4f59ac3b4436e2932aae";
-
-  // Gets the recipes matching the input term
   const getRecipes = async () => {
-    try {
-      const res = await axios.get(
-        "https://api.spoonacular.com/recipes/complexSearch",
-        {
-          params: {
-            apiKey: SPOONACULAR_API_KEY,
-            query: keyword,
-            diet,
-            excludeIngredients: exclude,
-          },
-        }
-      );
+    const apiKey = "YOUR_API_KEY";
+    const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${keyword}&diet=${diet}&excludeIngredients=${exclude}`;
 
-      const { data } = res;
-      setResponse(data.results);
+    try {
+      const response = await axios.get(apiUrl);
+      setResponse(response.data.results);
     } catch (error) {
-      console.error(error);
+      console.error("Error fetching data: ", error);
     }
   };
 
-  // Function to get specific recipe information by ID
-  const getRecipeInfo = async (recipeId) => {
-    try {
-      const res = await axios.get(
-        `https://api.spoonacular.com/recipes/${recipeId}/information`,
-        {
-          params: {
-            apiKey: SPOONACULAR_API_KEY,
-            includeNutrition: false,
-          },
-        }
-      );
-      const { data } = res;
-      // Extract the specific data you need
-      const { readyInMinutes, servings, sourceUrl } = data;
-
-      // Create an object with the extracted data
-      const recipeInfoData = {
-        readyInMinutes,
-        servings,
-        sourceUrl,
-      };
-
-      setRecipeInfo(recipeInfoData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  // Use the useEffect hook to fetch recipe information when response changes
   useEffect(() => {
-    if (response && response.length > 0) {
-      // Assuming you want to get information for the first recipe in the response
-      getRecipeInfo(response[0].id);
+    if (response) {
+      console.log(response);
     }
   }, [response]);
 
   return (
     <>
-     
-      <div className="d-flex align-items-center mx-2 my-5 ">
+      <div className="d-flex align-items-center mx-2 my-5">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-12 col-sm-8">
+            <div className="col-12 col-sm-6">
               <form
-                className="px-5 border border-black bg-success"
+                className="px-5"
                 onSubmit={(e) => {
                   e.preventDefault();
                   getRecipes();
                 }}
               >
-                <div className="my-3">
-                  <label htmlFor="recipe" className="form-label">
-                    Recipe
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Enter a recipe"
-                    value={keyword}
-                    onChange={(e) => setKeyword(e.target.value)}
-                  />
+                <div className="row justify-content-center">
+                  <div className="col-12 col-sm-6">
+                    <div className="my-3">
+                      <label htmlFor="recipe" className="form-label">
+                        Recipe
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter a recipe"
+                        value={keyword}
+                        onChange={(e) => setKeyword(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-12 col-sm-6">
+                    <div className="my-3">
+                      <label htmlFor="diet" className="form-label">
+                        Diet
+                      </label>
+                      <select
+                        id="diet"
+                        className="form-select"
+                        value={diet}
+                        onChange={(e) => setDiet(e.target.value)}
+                      >
+                        {["none", "pescetarian", "lacto vegetarian", "ovo vegetarian", "vegan", "vegetarian"].map((dietOption) => (
+                          <option key={dietOption} value={dietOption}>
+                            {dietOption}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
                 </div>
-
-                <div className="mb-3">
-                  <label htmlFor="diet" className="form-label">
-                    Diet
-                  </label>
-                  <select
-                    id="diet"
-                    className="form-select"
-                    value={diet}
-                    onChange={(e) => setDiet(e.target.value)}
-                  >
-                    {[
-                      "none",
-                      "pescetarian",
-                      "lacto vegetarian",
-                      "ovo vegetarian",
-                      "vegan",
-                      "vegetarian",
-                    ].map((dietOption) => (
-                      <option key={dietOption} value={dietOption}>
-                        {dietOption}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-
                 <div className="mb-3">
                   <label htmlFor="exclude" className="form-label">
                     Exclude Ingredients
@@ -134,8 +87,8 @@ const Home = () => {
                   />
                 </div>
 
-                <div className="mb-3 text-center">
-                  <button className="btn btn-warning col-8" type="submit">
+                <div className="searchButton mb-3 text-center">
+                  <button className="btn btn-primary col-8" type="submit">
                     Search
                   </button>
                 </div>
@@ -153,18 +106,15 @@ const Home = () => {
                 <img
                   src={recipe.image}
                   className="card-img-top"
-                  alt={recipe.id}
+                  alt={recipe.title}
                 />
                 <div className="card-body">
                   <h5 className="card-title">{recipe.title}</h5>
                   <p className="card-text">
-                    {recipeInfo &&
-                      recipeInfo.readyInMinutes &&
-                      recipeInfo.servings &&
-                      `Ready in ${recipeInfo.readyInMinutes} minutes -
-                      ${recipeInfo.servings} Servings`}
+                    {recipe.readyInMinutes && recipe.servings &&
+                      `Ready in ${recipe.readyInMinutes} minutes - ${recipe.servings} Servings`}
                   </p>
-                  <a href={recipeInfo && recipeInfo.sourceUrl}>Go to Recipe</a>
+                  <a href={recipe.sourceUrl}>Go to Recipe</a>
                 </div>
               </div>
             </div>
@@ -176,3 +126,4 @@ const Home = () => {
 };
 
 export default Home;
+
