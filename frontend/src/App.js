87ac358fd2aee5426b4ceee43a7fd3821cd03b13
tersiakoetzee <tersiakoetzee@gmail.com>
2024-02-09 17:38:39 +0200

    footer fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css"
 
 const App = () => {
   return (
